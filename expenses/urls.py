41041be305c4efbdbfa7654080050359ac06fa6e
Joshua Atoyebi <atoyebijoshua095@gmail.com>
2025-01-17 04:25:49 -0600

    Added link to detail page for mobile

--- expenses/urls.py
+++ expenses/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('<int:trip_id>/expenses/delete/<int:expense_id>/', views.delete_expense, name='delete-expense'),
     path('<int:trip_id>/download-expenses/', views.download_expenses_excel, name='download-expenses'),
     path('download-expenses-pdf/<int:trip_id>/', views.download_expenses_pdf, name='download-expenses-pdf'),
-    path('<int:trip_id>/expenses/<int:expense_id>/', views.expense_detail, name='expense-detail'),
+    path('<int:trip_id>/expenses/<int:expense_id>/', views.expense_detail, name='expense_detail'),
 ]
