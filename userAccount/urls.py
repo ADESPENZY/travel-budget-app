b0c069a16082dccb4a716499f111222ba4a7b96c
Joshua Atoyebi <atoyebijoshua095@gmail.com>
2025-01-30 02:21:57 -0600

    update

--- userAccount/urls.py
+++ userAccount/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.register_user, name='register-page'),
     path('login_user/', views.login_user, name='login-page'),
     path('logout_user/', views.logout_user, name='logout-page'),
+    
 ]
